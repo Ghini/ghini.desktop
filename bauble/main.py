7667556754c1389f221fe6250d4ea8f80bec1062
batoms <>
2006-09-30 19:53:28 +0000

    - updated deprecated gtk.threads_init,_enter,_leave to use gtk.gdk

--- bauble/main.py
+++ bauble/main.py
@@ -4,10 +4,10 @@ import bauble
 import gtk # this has to be after import bauble
 
 def bauble_main():
-    gtk.threads_init() # initialize threading
-    gtk.threads_enter()
+    gtk.gdk.threads_init() # initialize threading
+    gtk.gdk.threads_enter()
     bauble.app.main()
-    gtk.threads_leave()
+    gtk.gdk.threads_leave()
 
 if __name__ == "__main__":    
     bauble_main()
