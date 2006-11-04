477229b3b6e5221f16dbc09e7786c655451af2aa
batoms <>
2006-11-04 03:37:58 +0000

    - added test files to repository even though they don't have any test code in them at the moment

---
+++ bauble/plugins/formatter/test.py
@@ -0,0 +1,13 @@
+import gtk
+import gtk.glade
+import bauble.plugins.formatter as plugin
+
+#tool = plugin.FormatterTool.start()
+#dialog = plugin.FormatterDialog()        
+#formatter = dialog.start()
+#print formatter
+
+
+
+
+
