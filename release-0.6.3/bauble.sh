4452599417353f18a8bee34db0f32e28e4ed1e57
batoms <>
2006-12-03 09:32:21 +0000

    - fix a screw up where i copied a repository directory to the wrong place

--- release-0.6.3/bauble.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-#export PYTHONPATH=`dirname $0`
-SCRIPT_PATH=`dirname $0`/scripts/bauble
-PYTHONPATH=`dirname $0` exec python $SCRIPT_PATH
--- release-0.6.3/scripts/gen_search_doc.py
+++
@@ -1,4 +0,0 @@
-# generate documentation about searching
-# - the list of domains and their relevant tables
-# - link to the model doc for the columns to use or combine the two
-
