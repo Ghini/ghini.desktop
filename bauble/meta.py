da49c1b5fadf2754a1641ae4b86c6432c3a5c96c
batoms <>
2007-01-17 12:23:27 +0000

    - implemented basics of plugin registry system

--- bauble/meta.py
+++ bauble/meta.py
@@ -5,6 +5,7 @@ from sqlalchemy import *
 
 VERSION_KEY='version'
 CREATED_KEY='created'
+REGISTRY_KEY='registry'
 
 bauble_meta_table = Table('bauble',
                           Column('id', Integer, primary_key=True),
@@ -14,11 +15,19 @@ bauble_meta_table = Table('bauble',
                           Column('_last_updated', DateTime, default=func.current_timestamp(), 
                                  onupdate=func.current_timestamp()))
         
-class BaubleMetaTable(object):
+class BaubleMeta(object):
+    
     version = 'version'
     created = 'created'
-        
+    
+    def __init__(self, name=None, value=None):
+        self.name = name
+        self.value = value
+
+
     def __str__(self):
-        return '%s=%s' % (self.name, self.value
-                          )
-mapper(BaubleMetaTable, bauble_meta_table)
+        return '%s=%s' % (self.name, self.value)
+
+
+
+mapper(BaubleMeta, bauble_meta_table)
