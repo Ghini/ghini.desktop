c4ba231e0ac2f527f98a75ba3ceeed8735fd46ef
batoms <>
2007-04-17 00:14:03 +0000

    - add script used to make icon.ico from icon.svg - add icon.ico

---
+++ scripts/make_ico.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import sys, os
+
+if sys.platform != 'linux2':
+    print 'This script only works on Linux.'
+    sys.exit()
+
+
+
+sizes = ('16x16', '32x32', '48x48')
+image_src = 'bauble/images/icon.svg'
+out_template = 'bauble/images/icon%s.png'
+icon_filename = 'bauble/images/icon.ico'
+convert = lambda size: os.system('convert %s -resize %s %s' % \
+                                 (image_src, size, out_template % size))
+
+for size in sizes:
+    convert(size)
+
+os.system('icotool -c %s -o %s' % \
+          (' '.join([out_template % s for s in sizes]), icon_filename))
+
+
