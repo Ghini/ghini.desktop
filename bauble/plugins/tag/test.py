e3a3f2adc2296c6a4fe292b6764f0e288e284474
Brett Atoms <brettatoms@gmail.com>
2008-01-02 10:24:40 -0600

    - make tagged_obj store the entire class name with the module included so   that we can import the class later, that way we don't have to call   register_mapping for everything that might be tagged, this breaks   existing pre-0.8 tags - create tag tests

--- bauble/plugins/tag/test.py
+++ bauble/plugins/tag/test.py
@@ -1,37 +1,120 @@
+import unittest
+from sqlalchemy import *
+from sqlalchemy.exceptions import *
+from testbase import BaubleTestCase, log
+import bauble.utils as utils
+import bauble.plugins.tag as tag_plugin
+from bauble.plugins.tag import Tag, tag_table
+from bauble.plugins.plants import Family, family_table, Genus, genus_table, \
+     Species, species_table, VernacularName, vernacular_name_table
+from bauble.plugins.garden import Accession, accession_table, Plant, \
+     plant_table, Location, location_table
 
-#if __name__ == '__main__':
-#    # for testing
-#    sqlhub.processConnection = connectionForURI("sqlite:///tmp/test.sqlite")
-#    sqlhub.processConnection.getConnection()
-#
-#
-#    class Person(SQLObject):
-#        name = StringCol()
-#        def __str__(self):
-#            return self.name                
-#
-#    class Donkey(SQLObject):
-#        name = StringCol()    
-#        def __str__(self):
-#            return self.name
-#        
-#    tables = [Person, Donkey, TagCategory, Tag, TagObjId, TagIntermediate]
-#    def create_tables():
-#        for t in tables:
-#            t.dropTable(True)
-#            t.createTable()
-#    create_tables()
-#    
-#    p = Person(name='Ted')
-#    tag_object('human', p)
-#    tag_object('hawaiin', p)
-#
-#    d = Donkey(name='Crapper')
-#    tag_object('hawaiin', d)
-#
-#    for t in get_tags(p):
-#        print str(t)
-#        print '----------------'
-#        for id in t.ids:
-#            print '-- ' + str(id)
-#        print '\n'
+
+def setUp_test_data():
+    pass
+
+def tearDown_test_data():
+    pass
+
+
+class TagTestCase(BaubleTestCase):
+
+    def __init__(self, *args):
+        super(TagTestCase, self).__init__(*args)
+
+    def setUp(self):
+        super(TagTestCase, self).setUp()
+
+    def tearDown(self):
+        super(TagTestCase, self).tearDown()
+
+
+class TagTests(TagTestCase):
+
+
+    family_ids = [0, 1]
+
+    def setUp(self):
+        super(TagTests, self).setUp()
+        for f in self.family_ids:
+            family_table.insert({'id': f, 'family': str(f)}).execute()
+
+
+    def tearDown(self):
+        super(TagTests, self).tearDown()
+        self.session.bind.execute(family_table.delete())
+
+
+##     def test_get_tagged_objects(self):
+##         pass
+
+
+    def test_tag_objects(self):
+        tag_plugin.tag_objects('test', [self.session.load(Family, 0),
+                                        self.session.load(Family, 1)])
+        # get object by string
+        tagged_objs = tag_plugin.get_tagged_objects('test')
+        sorted_pairs= sorted([(type(o), o.id) for o in tagged_objs],
+                             cmp=lambda x, y: cmp(x[0], y[0]))
+        self.assert_(sorted_pairs == [(Family, 0), (Family, 1)], sorted_pairs)
+
+        # get object by tag
+        tag = self.session.query(tag_plugin.Tag).filter_by(tag='test').one()
+        tagged_objs = tag_plugin.get_tagged_objects(tag)
+        sorted_pairs= sorted([(type(o), o.id) for o in tagged_objs],
+                             cmp=lambda x, y: cmp(x[0], y[0]))
+        self.assert_(sorted_pairs == [(Family, 0), (Family, 1)], sorted_pairs)
+
+        tag_plugin.tag_objects('test', [self.session.load(Family, 0),
+                                        self.session.load(Family, 1)])
+
+        #
+        # now untag everything
+        #
+        tag_plugin.untag_objects('test', [self.session.load(Family, 0),
+                                          self.session.load(Family, 1)])
+        # get object by string
+        tagged_objs = tag_plugin.get_tagged_objects('test')
+        pairs = [(type(o), o.id) for o in tagged_objs]
+        self.assert_(pairs == [], pairs)
+
+        # get object by tag
+        tag = self.session.query(tag_plugin.Tag).filter_by(tag='test').one()
+        tagged_objs = tag_plugin.get_tagged_objects(tag)
+
+
+    def test_get_tag_ids(self):
+        fam0 = self.session.load(Family, 0)
+        fam1 = self.session.load(Family, 1)
+        tag_plugin.tag_objects('test', [fam0, fam1])
+        tag_plugin.tag_objects('test2', [fam0])
+
+        # test we only return the ids the objects have in common
+        sel = select([tag_table.c.id], tag_table.c.tag=='test')
+        test_id = [r[0] for r in sel.execute()]
+        ids = tag_plugin.get_tag_ids([fam0, fam1])
+        self.assert_(ids==test_id, ids)
+
+        # test that we return multiple tag ids if the objs share tags
+        tag_plugin.tag_objects('test2', [fam1])
+        sel = select([tag_table.c.id], or_(tag_table.c.tag=='test',
+                                           tag_table.c.tag=='test2'))
+        test_id = [r[0] for r in sel.execute()]
+        ids = tag_plugin.get_tag_ids([fam0, fam1])
+        self.assert_(ids==test_id, ids)
+
+
+class TagTestSuite(unittest.TestSuite):
+
+   def __init__(self):
+       super(TagTestSuite, self).__init__()
+       self.addTests(map(TagTests, ('test_tag_objects', 'test_get_tag_ids',
+                                    )))
+
+
+testsuite = TagTestSuite
+
+
+
+
