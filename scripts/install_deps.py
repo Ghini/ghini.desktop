1205fd783879936234f6a37f33b6e0c8649f82da
brettatoms@gmail.com <>
2008-01-26 17:38:08 -0600

    - added py2exe to install_deps.py

--- scripts/install_deps.py
+++ scripts/install_deps.py
@@ -58,7 +58,8 @@ EZ_SETUP_PATH = 'http://peak.telecommunity.com/dist/ez_setup.py'
 eggs_install = {'lxml': '==1.3.6',
                 'MySQL-python': '==1.2.2',
                 'simplejson': '==1.7.1', # 1.73 is the latest but doesn't have a compile win32 version on PPI
-                'SQLAlchemy': '>=0.4.2p3'}
+                'SQLAlchemy': '>=0.4.2p3',
+                'py2exe': '==0.6.6'}
 
 
 
