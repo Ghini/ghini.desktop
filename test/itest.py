9ccff6b654a2dcdfa2eea898b5189ccd3f7b4d03
Brett Atoms <brettatoms@gmail.com>
2008-03-12 08:40:21 -0600

    * test/itest.py: modernized itest.py a bit

--- test/itest.py
+++ test/itest.py
@@ -1,9 +1,10 @@
+#!/usr/bin/env python
 #
 # itest.py
 #
 # provides an interactive test environment
 #
-from sqlalchemy import *
+import sqlalchemy as sa
 from sqlalchemy.orm import *
 
 # TODO: should create another itest that will refect an existing
@@ -15,28 +16,50 @@ logging.basicConfig()
 
 uri = 'sqlite:///:memory:'
 
+metadata = sa.MetaData()
 
-metadata = MetaData()
+class MyTable(sa.Table):
 
-species_table = Table('species', metadata,
-                  Column('id', Integer, Sequence('species_id_seq'),
-                         primary_key=True),
-                  Column('sp', Text),
-                  Column('genus_id', Integer, ForeignKey('genus.id')),
-                  Column('default_vernacular_name_id', Integer))
-#                         ForeignKey('vernacular_name.id')))
+    def __init__(self, *args, **kwargs):
+        super(MyTable, self).__init__(*args, **kwargs)
+        self.append_column(sa.Column('_created', sa.DateTime(True),
+                                     default=sa.func.now()))
+        self.append_column(sa.Column('_last_updated', sa.DateTime(True),
+                                     default=sa.func.now(),
+                                     onupdate=sa.func.now()))
+
+species_table = sa.Table('species', metadata,
+                        sa.Column('id', sa.Integer,
+                                  sa.Sequence('species_id_seq'),
+                                  primary_key=True),
+                        sa.Column('sp', sa.Text, nullable=False),
+                        sa.Column('genus_id', sa.Integer,
+                                  sa.ForeignKey('genus.id')),
+                        sa.Column('default_vernacular_name_id', sa.Integer))
 
 
-vernacular_name_table = Table('vernacular_name', metadata,
-                              Column('id', Integer, primary_key=True),
-                              Column('name', Unicode(128)),
-                              Column('language', Unicode(128)),
-                              Column('species_id', Integer,
-                                     ForeignKey('species.id')))
+vernacular_name_table = sa.Table('vernacular_name', metadata,
+                                 sa.Column('id', sa.Integer, primary_key=True),
+                                 sa.Column('name', sa.Unicode(128),
+                                           nullable=False),
+                                 sa.Column('species_id', sa.Integer,
+                                           sa.ForeignKey('species.id')))
 
-genus_table = Table('genus', metadata,
-                    Column('id', Integer, primary_key=True),
-                    Column('genus', Text))
+genus_table =  sa.Table('genus', metadata,
+                        sa.Column('id', sa.Integer, primary_key=True),
+                        sa.Column('genus', sa.Text, nullable=False))
+
+#for table in [species_table, vernacular_name_table, genus_table]:
+for table in [genus_table, species_table]:
+##    continue
+#    default = 'today'
+    default = sa.func.lower('today')
+#    default = sa.func.now()
+#    default = None
+    table.append_column(sa.Column('_created',
+                                  sa.String(100),
+                                  #sa.DateTime(True),
+                                  default=default))
 
 class Genus(object):
     def __str__(self):
@@ -51,41 +74,70 @@ class VernacularName(object):
         return self.name
 
 species_mapper = mapper(Species, species_table,
-       properties = {'vn': relation(VernacularName, lazy=False,
-                                     backref=backref('species'))})
+        properties = {'vn': relation(VernacularName, lazy=False, uselist=False,
+                                     cascade='all, delete-orphan',
+                                     #backref=backref('species')
+                                     )})
 genus_mapper = mapper(Genus, genus_table,
        properties = {'species': relation(Species, lazy=False,
-                                         backref=backref('genus'))})#, order_by=['sp'])})
+                                         backref=backref('genus',
+                                                cascade='all, delete-orphan')
+                                         )})#, order_by=['sp'])})
 mapper(VernacularName, vernacular_name_table)
 
-#global_connect(uri)
-engine = create_engine(uri)
+engine = sa.create_engine(uri)
 engine.connect()
 metadata.bind = engine
-session = create_session()
+#session = create_session()
+Session = sessionmaker(bind=engine, autoflush=False, transactional=True)
+session = Session()
 
 metadata.create_all()
 genus_table.insert().execute({'genus': 'Maxillaria'})
-species_table.insert().execute({'genus_id': 1, 'sp': 'sp'})
+#species_table.insert().execute({'genus_id': 1, 'sp': 'sp'})
 
 #logging.getLogger('sqlalchemy').setLevel(logging.DEBUG)
 logging.getLogger('sqlalchemy').setLevel(logging.WARNING)
 
-#
+# *******************
 # where the action is
-#
+# *******************
 
-print '*********** query *********'
-g = session.query(Genus).get_by(id=1)
-print '*********** genus *********'
-print str(g)
+#g = session.load(Genus, 1)
+#s = session.load(Species, 1)
+g = Genus()
+s = Species()
+session.save(g)
+session.save(s)
+g.genus = "Genus"
+s.genus = g
+s.sp = "sp"
 
-print '*********** sp **************'
-print str(g.species)
+vn = VernacularName()
+session.save(vn)
+s.vn = vn
 
-print '********* vernacular ***********'
-print str(g.species[0].vn)
-
-
-
-
+def print_state(obj, sess):
+    print '%s: %s' % (obj, [obj in sess.new, obj in sess.dirty, obj in sess.deleted])
+try:
+    print_state(g, session)
+    session.commit()
+except:
+    print_state(g, session)
+    session.rollback()
+vn.name = u"something"
+## session.clear()
+## session.save(g)
+## session.save(s)
+## session.save(vn)
+session.expunge(g)
+session.expunge(s)
+#session.expunge(vn)
+session.flush()
+session.save(g)
+session.save(s)
+session.save(vn)
+#print_state(g, session)
+session.commit()
+#print_state(g, session)
+print s
