8f4c4d8701b76fd54ce28d93df0a84b7b840b77f
Brett Atoms <brettatoms@gmail.com>
2008-06-10 15:10:39 -0600

    - convert vernacular name and language to utf8 before creating the   VernacularName object in the species editor...removes unicode warning - code cleanups

--- bauble.sh
+++ bauble.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 #export PYTHONPATH=`dirname $0`
 SCRIPT_PATH=`dirname $0`/scripts/bauble
+# TODO: for some reason this doesn't really load the version of bauble
+# in PYTHONPATH, it will load the installed (/usr/lib/pythonX.Y)
+# version first....why?
 PYTHONPATH=`dirname $0` exec python $SCRIPT_PATH
