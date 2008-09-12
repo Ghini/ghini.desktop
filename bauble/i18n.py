ff76d98be0e4ed4ca9631e77393032ffdfc347ec
Brett Atoms <brettatoms@gmail.com>
2008-09-12 16:47:59 -0600

    - bumped version to 0.8.6 - first pass on getting translations to work

--- bauble/i18n.py
+++ bauble/i18n.py
@@ -4,13 +4,32 @@
 # internationalization support
 #
 
+import sys
+import os
 import locale
 import gettext
-import bauble.paths as paths
 
-__all__ = ["_"]
 
+__all__ = ["_", '_locale_dir']
+
+def _locale_dir():
+    """
+    Returns the root path of the locale files
+    """
+    if sys.platform == 'linux2':
+        return os.path.join('usr', 'share', 'locale')
+    elif sys.platform == 'win32':
+        return os.path.join(main_dir(), 'locale')
+    else:
+        raise NotImplementedError('This platform does not support '\
+                                  'translations')
+
+APP_NAME = 'bauble'
 locale.setlocale(locale.LC_ALL, '')
-gettext.bindtextdomain('bauble', paths.locale_dir())
-gettext.textdomain('bauble')
+gettext.bindtextdomain(APP_NAME, _locale_dir())
+gettext.textdomain(APP_NAME)
+f = gettext.find(APP_NAME)#, _locale_dir())
+print f
+# glade internationalization is setup in bauble/__init__.py after gtk
+# is successfully imported
 _ = gettext.gettext
--- scripts/build.nsi
+++ scripts/build.nsi
@@ -14,7 +14,7 @@ Name "Bauble"
 
 ; this should be the Bauble version number combined with the subversion 
 ; revision
-!define version "0.8.5"
+!define version "0.8.6"
 !define src_dir "../dist"
 Outfile "bauble-${version}-setup.exe"
 
