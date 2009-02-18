312358709d1f5f2ca87a4d3529e4a227a4d1cce4
Brett Atoms <brettatoms@gmail.com>
2009-02-18 12:56:26 -0600

    - added script to generate message template (.pot)

---
+++ scripts/i18n.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Extract all the translatable strings and generate the message template
+
+# Extract all the translatable strings from the glade files
+find ./bauble -name "*.glade" -exec intltool-extract --type=gettext/glade {} \;
+
+# Create the message.pot file from the python and .glade.h files
+xgettext -k_ -kN_ -o messages.pot `find ./bauble -name "*.py" -o -name *.h`
+
+#
+# Use the following command to create the .mo files for each
+# locale. This is done when the project is built.
+#
+# msgfmt po/en.po -o build/share/locale/en/LC_MESSAGES/bauble.mo
+
