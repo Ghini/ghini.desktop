d35643bac9ba0ec5cfa95faf840090789eb7bebf
Brett Atoms <brettatoms@launchpad.net>
2009-06-24 12:50:15 -0400

    - more fixes to avoid deadlocks for bauble.meta.get_default()

--- bauble/meta.py
+++ bauble/meta.py
@@ -25,7 +25,9 @@ def get_default(name, default=None, session=None):
     """
     Get a BaubleMeta object with name.  If the default value is not
     None then a BaubleMeta object is returned with name and the
-    default value given.  If a session instance is passed then we
+    default value given.
+
+    If a session instance is passed (session != None) then we
     don't commit the session.
     """
     commit = False
@@ -37,13 +39,16 @@ def get_default(name, default=None, session=None):
     if not meta and default is not None:
         meta = BaubleMeta(name=utils.utf8(name), value=default)
         session.add(meta)
+        if commit:
+            session.commit()
+            # load the properties so that we can close the session and
+            # avoid getting errors when accessing the properties on the
+            # returned meta
+            meta.value
+            meta.name
+
     if commit:
-        session.commit()
-        # load the properties so that we can close the session and
-        # avoid getting errors when accessing the properties on the
-        # returned meta
-        meta.value
-        meta.name
+        # close the session whether we added anything or not
         session.close()
     return meta
 
