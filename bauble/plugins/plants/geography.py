b8fa3c75afce19f866b6cbad027b1b13f70967a4
Brett Atoms <brettatoms@launchpad.net>
2009-10-07 20:12:39 -0600

    - fix getting the species from from a geography object

--- bauble/plugins/plants/geography.py
+++ bauble/plugins/plants/geography.py
@@ -12,36 +12,32 @@ from bauble.view import ResultSet
 from bauble.utils.log import debug
 
 
-def get_species_in_geography(geo):
-    # get all the geography children under geo
-    from bauble.plugins.plants.species import SpeciesDistribution, \
-         species_distribution_table, Species, species_table
-    geo_kids = []
-    # TODO: getting the kids is too slow, is there a better way?
-    # TODO: it might be better to put this in a tasklet or something so
-    # that we can atleast do a set_busy() on the gui so the user only clicks
-    # on the item once, or just disable double clicking until everything has
-    # expanded properly....that's probably not a bad idea in general, just
-    # before we get an items children in the results view we should disable
-    # clicking in the view and once the item has expanded we reenable it
-    #
-    # TODO: need to change this to use the new queries on the mapper
-    # or to update the select statements if we don't want to create
-    # full on Geography objects
-    raise NotImplementedError
-    kids_stmt = select([geography_table.c.id],
-                       geography_table.c.parent_id==bindparam('parent_id'))
-    def get_kids(parent_id):
-        for kid_id, in kids_stmt.execute(parent_id=parent_id):
-            geo_kids.append(kid_id)
-            get_kids(kid_id)
-        geo_kids.append(parent_id)
-    get_kids(geo.id)
+def get_species_in_geography(geo):#, session=None):
+    """
+    Return all the Species that have distribution in geo
+    """
     session = object_session(geo)
-    species_ids = select([species_distribution_table.c.species_id],
-                    species_distribution_table.c.geography_id.in_(geo_kids))
-    return ResultSet(session.query(Species).\
-                     filter(species_table.c.id.in_(species_ids)))
+    if not session:
+        ValueError('get_species_in_geography(): geography is not in a session')
+
+    # get all the geography children under geo
+    from bauble.plugins.plants.species_model import SpeciesDistribution, \
+        Species
+    # get the children of geo
+    geo_table = geo.__table__
+    master_ids = set([geo.id])
+    def get_geography_children(parent_id):
+        stmt = select([geo_table.c.id], geo_table.c.parent_id==parent_id)
+        kids = [r[0] for r in db.engine.execute(stmt).fetchall()]
+        for kid in kids:
+            grand_kids = get_geography_children(kid)
+            master_ids.update(grand_kids)
+        return kids
+    geokids = get_geography_children(geo.id)
+    master_ids.update(geokids)
+    q = session.query(Species).join(SpeciesDistribution).\
+        filter(SpeciesDistribution.geography_id.in_(master_ids))
+    return list(q)
 
 
 
