81b83834955c9584c901945d90478028e0339ae3
Brett Atoms <brettatoms@launchpad.net>
2010-02-09 18:36:28 -0500

    - bump version to 1.0.0b11

--- bauble/__init__.py
+++ bauble/__init__.py
@@ -9,7 +9,7 @@ import imp, os, sys
 import bauble.paths as paths
 
 # major, minor, revision version tuple
-version = '1.0.0b10' # :bump
+version = '1.0.0b11' # :bump
 """The Bauble version.
 """
 version_tuple = version.split('.')
--- scripts/build.nsi
+++ scripts/build.nsi
@@ -12,7 +12,7 @@
 ; general
 Name "Bauble - UBC"
 
-!define version "1.0.0b10" ; :bump
+!define version "1.0.0b11" ; :bump
 !define src_dir "../dist"
 Outfile "bauble-ubc-${version}-setup.exe"
 
