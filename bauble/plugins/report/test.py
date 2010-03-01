30e0b2491012b1acdcc2a0d6a9e809808fe028bf
Brett Atoms <brettatoms@launchpad.net>
2010-02-28 20:49:48 -0500

    - add required geography_id to collection in garden test data - add required quantity to plant in report test data

--- bauble/plugins/report/test.py
+++ bauble/plugins/report/test.py
@@ -80,7 +80,7 @@ class ReportTests(ReportTestCase):
                             loc = Location(id=pctr, code=u'%s' % pctr,
                                            name=u'site%s' % pctr)
                             plant = Plant(id=pctr, accession=acc, location=loc,
-                                          code=u'%s' % pctr)
+                                          code=u'%s' % pctr, quantity=1)
                             #debug('fctr: %s, gctr: %s, actr: %s, pctr: %s' \
                             #      % (fctr, gctr, actr, pctr))
                             self.session.add_all([loc, plant])
