2e4019c235835856feafc26cf058788ee8f242c7
Brett Atoms <brettatoms@launchpad.net>
2010-12-06 20:29:59 -0500

    - bumped version to 1.0.0b3

--- bauble/__init__.py
+++ bauble/__init__.py
@@ -9,7 +9,7 @@ import imp, os, sys
 import bauble.paths as paths
 
 # major, minor, revision version tuple
-version = '1.0.0b2' # :bump
+version = '1.0.0b3' # :bump
 """The Bauble version.
 """
 version_tuple = version.split('.')
