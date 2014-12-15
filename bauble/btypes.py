a550bc4be7a88beb7160fd60d99ac0d431c09aeb
Mario Frasca <mrgsfrasca@gmail.com>
2014-12-15 07:59:03 -0500

    pep8 compliance

--- bauble/btypes.py
+++ bauble/btypes.py
@@ -1,6 +1,21 @@
 #
 # types.py
 #
+# This file is part of bauble.classic.
+#
+# bauble.classic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# bauble.classic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bauble.classic. If not, see <http://www.gnu.org/licenses/>.
+
 import datetime
 import re
 
@@ -9,8 +24,6 @@ import sqlalchemy.types as types
 import sqlalchemy.exc as exc
 
 import bauble.error as error
-#import bauble.prefs as prefs
-#from bauble.utils.log import debug
 
 # TODO: store all times as UTC or support timezones
 
