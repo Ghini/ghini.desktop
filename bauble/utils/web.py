84750ffbeb0f4fff9cb7474f5ede5375b124a827
Mario Frasca <mrgsfrasca@gmail.com>
2014-12-31 08:21:17 -0500

    updated link. closes #17.

--- bauble/utils/web.py
+++ bauble/utils/web.py
@@ -74,7 +74,7 @@ class GoogleButton(StringLinkButton):
 
 class GBIFButton(StringLinkButton):
 
-    _base_uri = "http://data.gbif.org/search/%s"
+    _base_uri = "http://www.gbif.org/species/search/%s"
     _space = '+'
 
     def __init__(self, title=_("Search GBIF"), tooltip=_(
