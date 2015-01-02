6fc0890201cce6bd58e00a2f1a43a0ad341c0f53
Mario Frasca <mrgsfrasca@gmail.com>
2015-01-02 15:12:02 -0500

    ignore absence of gtk when testing

---
+++ bauble/fake_gtk.py
@@ -0,0 +1,9 @@
+class fake:
+    MESSAGE_ERROR = ''
+    MESSAGE_WARNING = ''
+    VBox = object
+    Action = Expander = ScrolledWindow = object
+    EventBox = object
+
+gtk = fake()
+gobject = fake()
