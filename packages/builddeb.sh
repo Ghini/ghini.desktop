ec541de8a01ccee17f0bdd1ed801da6f30eef89e
Mario Frasca <mario@anche.no>
2015-05-04 17:23:48 -0500

    bauble-1.0 now has 1.0.14. next pull from master will contain 1.0.15

--- packages/builddeb.sh
+++ packages/builddeb.sh
@@ -3,7 +3,7 @@
 # Requires bzr, devscripts, debhelper packages
 
 TOPLEVEL=`pwd`
-VERSION="1.0.14" # :bump
+VERSION="1.0.15" # :bump
 TARBALL="bauble-$VERSION.tar.gz"
 ORIG_TARBALL="bauble_$VERSION.orig.tar.gz"
 
