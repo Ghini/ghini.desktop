ab86d90fbc98dc506521a1a689f6b3e97e51713a
Mario Frasca <mario@anche.no>
2015-05-06 09:09:57 -0500

    the development branch is now 'master', in the Bauble organization.

--- scripts/devinstall.sh
+++ scripts/devinstall.sh
@@ -7,18 +7,18 @@ export PROJECT_HOME=$HOME/Devel
 source /usr/share/virtualenvwrapper/virtualenvwrapper.sh
 EOF
 . ~/.profile
-mkdir -p ~/Local/github/mfrasca
-cd ~/Local/github/mfrasca
-git clone https://github.com/mfrasca/bauble.classic
+mkdir -p ~/Local/github/Bauble
+cd ~/Local/github/Bauble
+git clone https://github.com/Bauble/bauble.classic
 mkvirtualenv bacl --system-site-packages
 workon bacl
 python setup.py build
 python setup.py install
-mkdir ~/bin
+mkdir ~/bin 2>/dev/null
 cat <<EOF > ~/bin/bauble
 #!/bin/bash
 
-GITHOME=$HOME/Local/github/mfrasca/bauble.classic/
+GITHOME=$HOME/Local/github/Bauble/bauble.classic/
 
 source /usr/local/bin/virtualenvwrapper.sh
 workon bacl
