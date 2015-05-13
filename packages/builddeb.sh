d90c722b0633f7787764c57554819c2aa3e81527
Mario Frasca <mario@anche.no>
2015-05-13 08:47:11 -0500

    bump to 1.0.19

--- packages/builddeb.sh
+++ packages/builddeb.sh
@@ -3,7 +3,7 @@
 # Requires bzr, devscripts, debhelper packages
 
 TOPLEVEL=`pwd`
-VERSION="1.0.17" # :bump
+VERSION="1.0.19" # :bump
 TARBALL="bauble-$VERSION.tar.gz"
 ORIG_TARBALL="bauble_$VERSION.orig.tar.gz"
 
