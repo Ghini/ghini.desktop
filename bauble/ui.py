fc35c3c68ad1e3846181f08aa098999a0ed01c81
Mario Frasca <mario@anche.no>
2015-05-27 16:53:33 -0500

    some cleaning up in unicode matters. (were causing warnings in tests).

--- bauble/ui.py
+++ bauble/ui.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2008-2010 Brett Adams
+# Copyright 2015 Mario Frasca <mario@anche.no>.
+#
+# This file is part of bauble.classic.
+#
+# bauble.classic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# bauble.classic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bauble.classic. If not, see <http://www.gnu.org/licenses/>.
 #
 # ui.py
 #
@@ -562,10 +581,10 @@ class GUI(object):
         try:
             engine = db.open(uri, True, True)
         except Exception, e:
-            # we don't do anything to handle the exception since
-            # db.open() should have shown an error dialog if there was
-            # a problem opening the database as long as the
-            # show_error_dialogs parameter is True
+            # we don't do anything to handle the exception since db.open()
+            # should have shown an error dialog if there was a problem
+            # opening the database as long as the show_error_dialogs
+            # parameter is True
             logger.warning(e)
 
         if engine is None:
