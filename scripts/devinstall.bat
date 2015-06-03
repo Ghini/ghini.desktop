4f06dd6cc951dd9c95a105022c25bb530aa66a4f
Mario Frasca <mario@anche.no>
2015-06-03 11:08:15 -0500

    cleaning up and extending devinstall scripts, to allow checkout of different production line.

--- scripts/devinstall.bat
+++ scripts/devinstall.bat
@@ -1,4 +1,13 @@
 @echo off
+IF %1.==. GOTO DEFAULTCHOICE
+set CHECKOUT=bauble-%1
+GOTO CONTINUE
+
+:DEFAULTCHOICE
+set CHECKOUT=bauble-1.0
+
+:CONTINUE
+echo going to install %CHECKOUT%
 cd "%HOMEDRIVE%%HOMEPATH%"
 pip install virtualenv 2>NUL
 virtualenv --system-site-packages .virtualenvs\bacl
@@ -7,7 +16,7 @@ mkdir Local\github\Bauble 2>NUL
 cd Local\github\Bauble
 git clone https://github.com/Bauble/bauble.classic.git
 cd bauble.classic
-git checkout bauble-1.0
+git checkout %CHECKOUT%
 python setup.py build
 python setup.py install
 mkdir "%APPDATA%\Bauble"
