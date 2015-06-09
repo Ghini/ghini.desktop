6d9f3b59439b1692e79510ddece7126e200e6f0b
Mario Frasca <mario@anche.no>
2015-06-09 09:40:21 -0500

    pep8 and removed strings with trailing space from terms to be translated.

--- bauble/plugins/imex/__init__.py
+++ bauble/plugins/imex/__init__.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2015 Mario Frasca <mario@anche.no>.
+#
+# This file is part of bauble.classic.
+#
+# bauble.classic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# bauble.classic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bauble.classic. If not, see <http://www.gnu.org/licenses/>.
 #
 # imex plugin
 #
@@ -7,15 +25,9 @@
 # TODO: would be best to provide some intermediate format so that we could
 # transform from any format to another
 
-import os, csv, traceback
-from sqlalchemy import *
-import bauble
-import bauble.utils as utils
 import bauble.pluginmgr as pluginmgr
-import bauble.task
-import Queue
 from bauble.plugins.imex.csv_ import CSVImportTool, CSVExportTool, \
-     CSVExportCommandHandler, CSVImportCommandHandler
+    CSVExportCommandHandler, CSVImportCommandHandler
 from bauble.plugins.imex.iojson import JSONImportTool, JSONExportTool
 from bauble.plugins.imex.xml import XMLExportTool, XMLExportCommandHandler
 
@@ -30,8 +42,9 @@ from bauble.plugins.imex.xml import XMLExportTool, XMLExportCommandHandler
 # table.insert().execute(*list) statement or it will fill in values for
 # missing columns so that all columns will have some value
 
+
 class ImexPlugin(pluginmgr.Plugin):
-    tools = [CSVImportTool, CSVExportTool, 
+    tools = [CSVImportTool, CSVExportTool,
              JSONImportTool, JSONExportTool, XMLExportTool]
     commands = [CSVExportCommandHandler, CSVImportCommandHandler,
                 XMLExportCommandHandler]
