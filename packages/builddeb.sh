692c279cfdb74b525e9562da040748e0236c89e0
Mario Frasca <mario@anche.no>
2015-07-03 07:20:32 -0500

    bumping to 1.0.44

--- packages/builddeb.sh
+++ packages/builddeb.sh
@@ -3,7 +3,7 @@
 # Requires bzr, devscripts, debhelper packages
 
 TOPLEVEL=`pwd`
-VERSION="1.0.43" # :bump
+VERSION="1.0.44" # :bump
 TARBALL="bauble-$VERSION.tar.gz"
 ORIG_TARBALL="bauble_$VERSION.orig.tar.gz"
 
