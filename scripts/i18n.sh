3747f7927070fe6cfbc89a6c4cc06234b120890f
Mario Frasca <mario@anche.no>
2015-09-03 08:03:04 -0500

    more verbose behaviour, use pygettext, exlude (generated) api.rst.

--- scripts/i18n.sh
+++ scripts/i18n.sh
@@ -10,25 +10,39 @@ mkdir -p pot
 #------------------------------------------------------------------------
 
 # glade
+echo -n 'doing glade files ... '
 xgettext --language=glade -o pot/glade.pot --from-code=utf-8 $(find bauble -name "*.glade")
+echo 'ok'
 
 # python
-xgettext --language=Python -o pot/python.pot --from-code=utf-8 -k_ $(find bauble -name "*.py")
+echo -n 'doing python files ... '
+pythonlist=$(mktemp)
+find bauble -name "*.py" > $pythonlist
+pygettext -X $pythonlist --escape -o pot/python.pot $(find bauble -name "*.py")
+sed -i -e 's/CHARSET/utf-8/' pot/python.pot  # I do not know how to specify it on command line
+echo 'ok'
 
-# documentation
+# documentation, but not the api.
+echo -n 'doing documentation files (excluding api.rst) ... '
 sphinx-build -b gettext doc pot/
+rm pot/api.pot
+echo 'ok'
 
 #------------------------------------------------------------------------
 # merge all message templates into one
 #------------------------------------------------------------------------
 touch pot/messages.pot
 rm pot/messages.pot
+echo -n 'now merging partial pot files ... '
 msgcat pot/*.pot > pot/messages.pot
+echo 'ok'
 
 #------------------------------------------------------------------------
 # finally update all internationalization files
 #------------------------------------------------------------------------
+echo -n 'finally update all internationalization files ... '
 for po in $(find po -name \*.po)
 do
     msgmerge -UNqs $po pot/messages.pot
 done
+echo 'ok'
