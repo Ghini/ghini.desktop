b9c6a330e2787f8c11eda7325ec7fb7ff201c1ef
Mario Frasca <mario@anche.no>
2015-09-09 14:08:36 -0500

    making a start at defining tests for the connection manager.

---
+++ bauble/test/test_connmgr.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2005,2006,2007,2008,2009 Brett Adams <brett@belizebotanic.org>
+# Copyright (c) 2012-2015 Mario Frasca <mario@anche.no>
+#
+# This file is part of bauble.classic.
+#
+# bauble.classic is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# bauble.classic is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with bauble.classic. If not, see <http://www.gnu.org/licenses/>.
+
+import os
+
+from nose import SkipTest
+
+from bauble.test import BaubleTestCase, check_dupids
+#from bauble.connmgr import ConnMgrPresenter
+
+
+def test_duplicate_ids():
+    """
+    Test for duplicate ids for all .glade files in the tag plugin.
+    """
+    import bauble.connmgr as mod
+    head, tail = os.path.split(mod.__file__)
+    assert(not check_dupids(os.path.join(head, 'connmgr.glade')))
