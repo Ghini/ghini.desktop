20459ac94d0ba20204358d6c6c8f5efa0a2ccd1c
Mario Frasca <mario@anche.no>
2015-12-12 17:21:29 -0500

    sorry, mistake. again relates to #167 and #215

--- scripts/devinstall.bat
+++ scripts/devinstall.bat
@@ -40,8 +40,8 @@ ECHO create the globalizing script
 IF DEFINED PUBLIC (SET AUDESKTOP=%PUBLIC%\Desktop) & (SET AUSTARTMENU=%PROGRAMDATA%\Microsoft\Windows\Start Menu) ELSE (SET AUDESKTOP=%ALLUSERSPROFILE%\Desktop) & (SET AUSTARTMENU=%ALLUSERSPROFILE%\Start Menu)
 (
 echo @echo off
-mkdir "%AUSTARTMENU%\Programs\Bauble"
-copy "%HOMEDRIVE%%HOMEPATH%"\Local\github\Bauble\bauble.classic\scripts\bauble.lnk "%AUSTARTMENU%\Programs\Bauble"
+echo mkdir "%AUSTARTMENU%\Programs\Bauble"
+echo copy "%HOMEDRIVE%%HOMEPATH%"\Local\github\Bauble\bauble.classic\scripts\bauble.lnk "%AUSTARTMENU%\Programs\Bauble"
 ) > devinstall-finalize.bat
 
 ECHO please run devinstall-finalize.bat as administrator.
