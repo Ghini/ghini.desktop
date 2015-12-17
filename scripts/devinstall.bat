409d482b5438e89b822a241b33a18fca41638d29
Mario Frasca <mario@anche.no>
2015-12-17 14:04:36 -0500

    relates to #215 (do something else if the user has no administrator rights)

--- scripts/devinstall.bat
+++ scripts/devinstall.bat
@@ -40,8 +40,12 @@ ECHO create the globalizing script
 IF DEFINED PUBLIC (SET AUDESKTOP=%PUBLIC%\Desktop) & (SET AUSTARTMENU=%PROGRAMDATA%\Microsoft\Windows\Start Menu) ELSE (SET AUDESKTOP=%ALLUSERSPROFILE%\Desktop) & (SET AUSTARTMENU=%ALLUSERSPROFILE%\Start Menu)
 (
 echo @echo off
-echo mkdir "%AUSTARTMENU%\Programs\Bauble"
-echo copy "%HOMEDRIVE%%HOMEPATH%"\Local\github\Bauble\bauble.classic\scripts\bauble.lnk "%AUSTARTMENU%\Programs\Bauble"
+echo NET SESSION >nul 2>&1
+echo IF %ERRORLEVEL% EQU 0 (
+echo  mkdir "%AUSTARTMENU%\Programs\Bauble"
+echo  copy "%HOMEDRIVE%%HOMEPATH%"\Local\github\Bauble\bauble.classic\scripts\bauble.lnk "%AUSTARTMENU%\Programs\Bauble"
+echo ) else (
+echo )
 ) > devinstall-finalize.bat
 
 ECHO please run devinstall-finalize.bat as administrator.
