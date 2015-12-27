0bd8178fca48819c5a4e779a497ba2cc9eaf6615
Mario Frasca <mario@anche.no>
2015-12-27 08:02:42 -0500

    work in progress, touches both #229 and #239

--- bauble/db.py
+++ bauble/db.py
@@ -257,6 +257,7 @@ def open(uri, verify=True, show_error_dialogs=False):
     from sqlalchemy.pool import NullPool, SingletonThreadPool
     from bauble.prefs import testing
     poolclass = testing and SingletonThreadPool or NullPool
+    poolclass = SingletonThreadPool
     new_engine = sa.create_engine(uri, echo=SQLALCHEMY_DEBUG,
                                   implicit_returning=False,
                                   poolclass=poolclass)
