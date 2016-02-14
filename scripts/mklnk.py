2b549f6c4d93751b09057f7a32b4f7d0698b1869
Mario Frasca <mario@anche.no>
2016-02-14 16:40:04 +0100

    propagating #51 into master

--- scripts/mklnk.py
+++ scripts/mklnk.py
@@ -3,8 +3,8 @@ import os
 
 base = os.path.expanduser("~\\Local\\github\\Ghini\\ghini.desktop")
 path = os.path.join(base, 'scripts\\ghini.lnk')
-target = os.path.join(base, "scripts\\bauble.vbs")
-wDir = os.path.expanduser("~")
+target = os.path.join(base, "scripts\\ghini.vbs")
+wDir = os.path.join(base, 'scripts')
 icon = os.path.join(base, "bauble\\images\\icon.ico")
 
 shell = Dispatch('WScript.Shell')
