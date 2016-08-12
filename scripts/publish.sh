945c7ee9bc277f4ba5df56bae7634c5bc2edcb70
Mario Frasca <mario@anche.no>
2016-08-12 07:13:30 -0500

    do not use git-up, but the standard and now functional fetch-all

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,19 @@
 #!/bin/sh
-git up
-git checkout ghini-1.0
-git merge ghini-1.0-dev
+LINE=ghini-1.0
+#### the following lines make sure you have locally all remote branches
+# git branch -r |
+#     grep -v '\->' |
+#     while read remote
+#     do
+#         git branch --track "${remote#origin/}" "$remote" 2>/dev/null
+#     done
+git fetch --all
+git checkout $LINE
+git merge $LINE-dev
 git commit -m "publish to 1.0"
 git push
-git checkout ghini-1.0-dev
-git up
+git checkout $LINE-dev
+git fetch --all
 scripts/bump_version.py +
 git commit -a -m "bumping version"
 git push
