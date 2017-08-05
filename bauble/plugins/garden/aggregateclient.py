df57010e0af857cced25ec49ca0846f19aea8384
Mario Frasca <mario@anche.no>
2017-08-05 12:19:54 -0500

    simplifying code

--- bauble/plugins/garden/aggregateclient.py
+++ bauble/plugins/garden/aggregateclient.py
@@ -1,3 +1,23 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017 Mario Frasca <mario@anche.no>.
+#
+# This file is part of ghini.desktop.
+#
+# ghini.desktop is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# ghini.desktop is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with ghini.desktop. If not, see <http://www.gnu.org/licenses/>.
+#
+
 from requests.auth import HTTPDigestAuth
 import requests
 import xml.etree.ElementTree as ET
@@ -11,7 +31,7 @@ def get_submissions(host, form_id, user, pw):
                                          'host': host},
                           auth=auth)
     root = ET.fromstring(result.text)
-    idlist, cursor = [child for child in root]
+    idlist, cursor = root
     result = []
     for uuid in [i.text for i in idlist]:
         url = (base_format % {'form_id': form_id,
@@ -21,7 +41,7 @@ def get_submissions(host, form_id, user, pw):
                                     'uuid': uuid})
         reply = requests.get(url, auth=auth)
         root = ET.fromstring(reply.text)
-        [data for data in root]
-        [form for form in data]
+        data, = root
+        form, = data
         result.append(dict([(i.tag.replace('{http://opendatakit.org/submissions}', ''), i.text) for i in form]))
     return result
