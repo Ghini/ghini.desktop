dfa23820dc165e10665aab4080f11f194c7a9454
Mario Frasca <mario@anche.no>
2017-08-06 19:31:14 -0500

    exporting to pocket, but no GUI and no tests

--- bauble/plugins/garden/exporttopocket.py
+++ bauble/plugins/garden/exporttopocket.py
@@ -21,19 +21,97 @@
 import logging
 logger = logging.getLogger(__name__)
 
+from bauble.plugins.garden.plant import Plant
 from bauble.plugins.garden.accession import Accession
-from bauble.plugins.garden.location import Location
-from bauble.plugins.garden.plant import PlantNote, Plant
-from bauble.plugins.garden.source import (Source, Contact)
 
 from bauble import db
 from bauble import pluginmgr
 from bauble.i18n import _
 
-def export_to_pocket():
-    from bauble.plugins.plants import Species, Genus, Family
+
+def create_pocket(filename):
+    create_sql = ['''\
+CREATE TABLE "android_metadata" (
+	"locale"	TEXT DEFAULT 'en_US'
+);''', '''\
+INSERT INTO "android_metadata" VALUES('en_US');''', '''\
+CREATE TABLE "species" (
+	"_id"	INTEGER,
+	"family"	TEXT,
+	"genus"	TEXT,
+	"epithet"	TEXT,
+	"sub-rank"	TEXT,
+	"sub-epithet"	TEXT,
+	"author"	TEXT,
+	PRIMARY KEY(_id)
+);''', '''\
+CREATE TABLE "accession" (
+	"_id"	INTEGER,
+	"code"	TEXT,
+	"species_id"	INTEGER,
+	"source"	TEXT,
+	PRIMARY KEY(_id)
+);''', '''\
+CREATE TABLE "plant" (
+	"_id"	INTEGER,
+	"accession_id"	INTEGER,
+	"code"	TEXT
+);''','''\
+''']
+    import sqlite3
+    cn = sqlite3.connect(filename)
+    cr = cn.cursor()
+    for statement in create_sql:
+        cr.execute(statement)
+    cn.commit()
+
+def export_to_pocket(filename, include_private=True):
+    from bauble.plugins.plants import Species
     session = db.Session()
-    
+    plant_query = (session.query(Plant)
+                   .order_by(Plant.code)
+                   .join(Accession)
+                   .order_by(Plant.id))
+    if include_private is False:
+        plant_query = (plant_query
+                       .filter(Accession.private == False))  # `is` does not work
+    plants = plant_query.all()
+    accessions = (session.query(Accession)
+                  .filter(Accession.id.in_([j.accession_id for j in plants]))
+                  .order_by(Accession.id).all())
+    species = (session.query(Species).
+               filter(Species.id.in_([j.species_id for j in accessions]))
+               .order_by(Species.id).all())
+    import sqlite3
+    cn = sqlite3.connect(filename)
+    cr = cn.cursor()
+    for s in species:
+        try:
+            cr.execute('INSERT INTO "species" '
+                   '(_id, family, genus, epithet, "sub-rank", "sub-epithet", author) '
+                   'VALUES (?, ?, ?, ?, ?, ?, ?);',
+                   (s.id, s.genus.family.epithet, s.genus.epithet, s.epithet,
+                    s.infraspecific_rank, s.infraspecific_epithet,
+                    s.infraspecific_author or s.sp_author or ''))
+        except Exception, e:
+            print type(e), e, s.id
+    for a in accessions:
+        try:
+            cr.execute('INSERT INTO "accession" '
+                   '(_id, code, species_id, source) '
+                   'VALUES (?, ?, ?, ?);',
+                   (a.id, a.code, a.species_id, ''))
+        except Exception, e:
+            print type(e), e, a.id
+    for p in plants:
+        try:
+            cr.execute('INSERT INTO "plant" '
+                   '(_id, accession_id, code) '
+                   'VALUES (?, ?, ?);',
+                   (p.id, p.accession_id, "." + p.code))
+        except Exception, e:
+            print type(e), e, p.id
+    cn.commit()
     session.close()
     return True
 
@@ -44,4 +122,6 @@ class ExportToPocketTool(pluginmgr.Tool):
 
     @classmethod
     def start(self):
-        export_to_pocket()
+        pocket = '/tmp/pocket.db'
+        create_pocket(pocket)
+        export_to_pocket(pocket)
