f317f2ab3ac0340a6d9fef9dc24e8b0234511db8
mfrasca <mario@anche.no>
2017-08-16 09:58:39 -0500

    correcting/updating copyright and authorship statement

--- doc/conf.py
+++ doc/conf.py
@@ -61,7 +61,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'Ghini'
 copyright = u'2004-2017 bauble.classic & 2015-2017 ghini.desktop contributors'
-author = u'Brett Adams; Mario Frasca'
+author = u'Mario Frasca'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
