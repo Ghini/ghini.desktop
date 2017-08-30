1d62123b25b8a169a3d3e5adc88298acba7dac29
mfrasca <mario@anche.no>
2017-08-30 15:11:37 -0500

    work in progress, related to #243

--- scripts/odk2json.py
+++ scripts/odk2json.py
@@ -33,39 +33,98 @@ import os.path
 import datetime
 import json
 import codecs
+import bauble.db
+import bauble.utils
+
+from bauble.plugins.garden import Location
+from bauble.plugins.garden import Plant
+from bauble.plugins.garden import Accession
+from bauble.plugins.plants import Species
+from bauble.plugins.plants import Genus
 
 import os
 path = os.path.dirname(os.path.realpath(__file__))
 with open(os.path.join(path, 'settings.json'), 'r') as f:
-    user, pw, filename, imei2user = json.load(f)
+    (user, pw, filename, imei2user, dburi, to_skip) = json.load(f)
 
-r = bauble.plugins.garden.aggregateclient.get_submissions(user, pw, 'ghini-collect.appspot.com', 'plant_form_r')
+r = bauble.plugins.garden.aggregateclient.get_submissions(user, pw, 'ghini-collect.appspot.com', 'plant_form_r', to_skip)
 objects = []
+species_needed = {}
+locations_needed = {}
 
-for item in r:
+bauble.db.open(dburi, True, True)
+session = bauble.db.Session()
+
+# loop over the submissions, sorted by accession number
+for item in sorted(r, key=lambda x: x['acc_no_scan'] or x['acc_no_typed']):
     accession = {"object": "accession"}
     plant = {"object": "plant", "code": "1"}
     accession['code'] = item['acc_no_scan'] or item['acc_no_typed']
     plant['accession'] = accession['code']
     if item['location']:
-        plant['location'] = item['location']
+        # correct location codes according to ILIKE matches,
+        db_loc = session.query(Location).filter(bauble.utils.ilike(Location.code, unicode(item['location']))).first()
+        if db_loc:
+            plant['location'] = db_loc.code
+        else:
+            plant['location'] = item['location'].upper()
+            locations_needed[plant['location']] = {'object': 'location', 'code': plant['location']}
+            
     if item['species']:
+        item['species'] = item['species'].replace('.', '')
+
+        genus_epithet, species_epithet = (unicode(item['species']).split(u' ') + [u''])[:2]
+        if species_epithet == '':
+            species_epithet = u'sp'
+
+        accession['species'] = item['species'] = u"%s %s" % (genus_epithet, species_epithet)
+
+    # add a default quantity=1 for plants relative to new accessions,
+    # add a default species=Zzz sp for new accessions,
+    # ignore species=Zzz sp for already existing accessions.
+    need_species = False
+    db_accession = session.query(Accession).filter(Accession.code == unicode(accession['code'])).first()
+
+    if db_accession is None:  # this is a new accession
+        plant['quantity'] = 1
+        item.setdefault('species', u'Zzz sp')
         accession['species'] = item['species']
+        genus_epithet, species_epithet = (unicode(item['species']).split(u' ') + [u''])[:2]
+        need_species = True
+    else:
+        if not item['species'] or db_accession.species.str(remove_zws=True) == item['species']:
+            accession = {}
+
+    if item['species']:
+        db_genus = session.query(Genus).filter(Genus.epithet == genus_epithet).first()
+        if db_genus is None:
+            logger.debug("com'è possibile? %s" % item['species'])
+        else:
+            db_species = session.query(Species).filter(
+                Species.genus_id == db_genus.id).filter(
+                Species.epithet == species_epithet).first()
+            if db_species is None:
+                species = {'object': 'taxon',
+                           'rank': 'species', 'epithet': species_epithet,
+                           'ht-rank': 'genus', 'ht-epithet': genus_epithet, }
+                species_needed[(genus_epithet, species_epithet)] = species
+
     # should create a change object, just like the Accession Editor:
     if True:
         author = imei2user[item['deviceid']]
         timestamp = datetime.datetime.strptime(item['end'][:19], '%Y-%m-%dT%H:%M:%S')
-        if author != 'Denisse':
-            logger.info("skipping user %s" % author)
-            continue
     # should import pictures:
     if False:
         for p in item['photo']:
             url, md5 = r[0]['media'][i]
             path = os.path.join('home', 'mario', 'tmp', 'PlantPictures', p)
             bauble.plugins.garden.aggregateclient.get_image(user, pw, url, path)
-    objects.append(accession)
+    if accession:
+        objects.append(accession)
     objects.append(plant)
+
+for i in species_needed.values() + locations_needed.values():
+    objects.insert(0, i)
 
 with codecs.open(filename, "wb", "utf-8") as output:
     output.write('[')
