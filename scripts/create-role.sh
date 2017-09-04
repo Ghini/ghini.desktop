d05681bce35094120e6c83452e85ff83067e36c1
mfrasca <mario@anche.no>
2017-09-04 16:33:52 -0500

    just polishing useless script.

--- scripts/create-role.sh
+++ scripts/create-role.sh
@@ -2,10 +2,11 @@
 
 USER=$1
 PASSWD=$2
+shift 2
 
-cat <<EOF | psql bauble -U bauble
-create role $1 with login encrypted password '$2';
-grant all privileges on all tables in schema public to $1;
-grant all privileges on all sequences in schema public to $1;
-grant all privileges on all functions in schema public to $1;
+cat <<EOF | psql bauble -U bauble $@
+create role $USER with login encrypted password '$PASSWD';
+grant all privileges on all tables in schema public to $USER;
+grant all privileges on all sequences in schema public to $USER;
+grant all privileges on all functions in schema public to $USER;
 EOF
