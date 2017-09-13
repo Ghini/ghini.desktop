3cc0aac9522c4356b91dc86c5b7e87e05187df54
mfrasca <mario@anche.no>
2017-09-13 16:54:27 -0500

    rewrite script, do not go through a json file.

--- scripts/searcheslog2json.py
+++ scripts/searcheslog2json.py
@@ -18,48 +18,105 @@
 # You should have received a copy of the GNU General Public License
 # along with ghini.desktop. If not, see <http://www.gnu.org/licenses/>.
 
-import csv
+import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+consoleHandler = logging.StreamHandler()
+logging.getLogger().addHandler(consoleHandler)
+consoleHandler.setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.DEBUG)
+
+import os.path
+path = os.path.dirname(os.path.realpath(__file__))
+
 import json
-import staale
 
-k = []
+with open(os.path.join(path, 'settings.json'), 'r') as f:
+    (user, pw, filename, imei2user, dburi, pic_path) = json.load(f)
+
+import bauble.db
+import bauble.utils
+
+from bauble.plugins.garden import Location
+from bauble.plugins.garden import Plant, PlantNote
+from bauble.plugins.garden import Accession
+from bauble.plugins.plants import Species
+from bauble.plugins.plants import Genus
+
+bauble.db.open(dburi, True, True)
+session = bauble.db.Session()
+
+q = session.query(Species).filter(Species.infrasp1 == u'sp')
+q = q.join(Genus).filter(Genus.epithet == u'Zzz')
+zzz = q.one()
+
+import sys
+
+import csv
+
 header = ['timestamp', 'loc', 'acc_code', 'binomial']
-
-# list of formats once data lines are read
-sp_format = ',\n{"object": "taxon", "rank": "species", "epithet": "%(sp_epit)s", "ht-rank": "genus", "ht-epithet": "%(gn_epit)s"}'
-acc_format = ',\n{"object": "accession", "code": "%(acc_code)s", "species": "%(binomial)s"}'
-plt_format = ',\n{"object": "plant", "accession": "%(acc_code)s", "code": "%(plt_code)s", %(plt_qty)s"location": "%(loc)s"}'
 
 input_file_name = '/tmp/searches.txt'
 
-count = skipped = 0
-
-species_collected = set(['Zzz sp'])
-old_accessions = set()
-
-with open("/tmp/out.json", "w") as out:
-    out.write('[ {}')
-    lastloc = None
-    for line_no, r in enumerate(csv.reader(open(input_file_name), delimiter=':')):
-        obj = dict(zip(header, [i.strip() for i in r]))
+with open(input_file_name) as searches_txt:
+    for line_no, r in enumerate(csv.reader(searches_txt, delimiter=':')):
+        obj = dict(zip(header, [unicode(i.strip()) for i in r]))
         if len(obj) == 1:
-            break
+            continue  # ignore blank lines
         obj.setdefault('binomial', 'Zzz sp')
         try:
             obj['gn_epit'], obj['sp_epit'] = obj['binomial'].split(' ')
         except:
             obj['gn_epit'], obj['sp_epit'] = ('Zzz', 'sp')
-        if obj['binomial'] not in species_collected:
-            out.write(sp_format % obj)
-            species_collected.add(obj['binomial'])
-        if obj['acc_code'] not in old_accessions:
-            out.write(acc_format % obj)
-            obj['plt_qty'] = '"quantity": 1, '
+
+        loc = session.query(Location).filter(Location.code == obj['loc']).one()
+
+        genus = session.query(Genus).filter(Genus.epithet == obj['gn_epit']).one()
+        if obj['sp_epit'] == u'sp':
+            try:
+                species = session.query(Species).filter(
+                    Species.genus == genus).filter(
+                    Species.infrasp1 == u'sp').first()
+                if species != zzz:  # no hace falta mencionarlo
+                    sys.stdout.write('+')  # encontramos
+            except:
+                species = Species(genus=genus, sp=u'', infrasp1=u'sp')
+                session.add(species)
+                session.flush()
+                if species != zzz:  # no hace falta mencionarlo
+                    sys.stdout.write('*')  # tuvimos que crear
         else:
-            obj['plt_qty'] = ''
-        if not obj['loc']:
-            obj['loc'] = lastloc
-        lastloc = obj['loc']
-        obj['plt_code'] = 1
-        out.write(plt_format % obj)
-    out.write(']')
+            try:
+                species = session.query(Species).filter(
+                    Species.genus == genus).filter(
+                    Species.epithet == obj['sp_epit']).one()
+                sys.stdout.write('+')  # encontramos
+            except:
+                species = Species(genus=genus, sp=u'', epithet=obj['sp_epit'])
+                session.add(species)
+                session.flush()
+                sys.stdout.write('*')  # tuvimos que crear
+
+        try:
+            q = session.query(Plant)
+            q = q.join(Accession).filter(Accession.code == obj['acc_code'])
+            q = q.filter(Plant.code == u'1')
+            plant = q.one()
+            plant.location = loc
+            sys.stdout.write('.')
+        except:
+            try:
+                accession = session.query(Accession).filter(Accession.code == obj['acc_code']).one()
+            except:
+                accession = Accession(species=species, code=obj['acc_code'])
+                session.add(accession)
+                sys.stdout.write('a')
+            plant = Plant(accession=accession, location=loc, quantity=1, code=u'1')
+            session.add(plant)
+            session.flush()
+            sys.stdout.write('p')
+
+
+print
+session.commit()
