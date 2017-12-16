4c1218bdae1f76e704211444d4042781c9d6c31d
mfrasca <mario@anche.no>
2017-12-16 13:31:30 -0500

    alter custom commit message to merge commit, so it looks more like the default one.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,7 +3,7 @@ LINE=ghini-1.0
 #### the following lines make sure you have locally all remote branches
 git remote update
 git checkout $LINE
-git merge $LINE-dev --no-edit -m "publish to $(grep :bump bauble/version.py | grep -o '[1-9]\.[0-9]\.[0-9]*')"
+git merge $LINE-dev --no-edit -m "Merge branch 'ghini-1.0-dev' into ghini-1.0, as $(grep :bump bauble/version.py | grep -o '[1-9]\.[0-9]\.[0-9]*')"
 git push
 git checkout $LINE-dev
 git fetch --all
