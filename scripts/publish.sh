5e3a25d8618e8d27a7233ff5f6f2ca8ce3df58ee
mfrasca <mario@anche.no>
2018-01-18 10:42:28 -0500

    read version from dev (that's the one about to be published), not from stable (that's the current one).

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,8 +2,9 @@
 LINE=ghini-1.0
 #### the following lines make sure you have locally all remote branches
 git remote update
+PUBLISHING=$(grep :bump bauble/version.py | grep -o '[1-9]\.[0-9]\.[0-9]*')
 git checkout $LINE
-git merge $LINE-dev --no-edit -m "Merge branch 'ghini-1.0-dev' into ghini-1.0, as $(grep :bump bauble/version.py | grep -o '[1-9]\.[0-9]\.[0-9]*')"
+git merge $LINE-dev --no-edit -m "Merge branch 'ghini-1.0-dev' into ghini-1.0, as $PUBLISHING"
 git push
 git checkout $LINE-dev
 git fetch --all
