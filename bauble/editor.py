28fc01a142381829b8571965ae25580ae52b815d
mfrasca <mario@anche.no>
2018-01-17 20:35:46 -0500

    we can invoke the callbacks using shortcuts, see #91.

--- bauble/editor.py
+++ bauble/editor.py
@@ -1091,16 +1091,21 @@ class GenericEditorPresenter(object):
                 self.refresh_view()
             view.connect_signals(self)
 
-            if False:
-                copy = gtk.Action('clip-copy', 'clip-copy', '', None)
-                paste = gtk.Action('clip-paste', 'clip-paste', '', None)
-                actions = gtk.ActionGroup('window-clip-actions')
-                actions.add_action_with_accel(copy, '<ctrl><shift>c')
-                actions.add_action_with_accel(paste, '<ctrl><shift>v')
-                copy.connect("activate", self.on_window_clip_copy)
-                paste.connect("activate", self.on_window_clip_paste)
-                for action in actions.list_actions():
-                    view.get_window().add_action(action)
+            actiongroup = gtk.ActionGroup('window-clip-actions')
+            accelgroup = gtk.AccelGroup()
+            fake_toolbar = gtk.Toolbar()
+            view.get_window().add_accel_group(accelgroup)
+            view.get_window().get_content_area().pack_start(fake_toolbar)
+            for shortcut, cb in (('<ctrl><shift>c', self.on_window_clip_copy),
+                                 ('<ctrl><shift>v', self.on_window_clip_paste)):
+                action = gtk.Action(shortcut, shortcut, 'clip-action', None)
+                actiongroup.add_action_with_accel(action, shortcut)
+                action.connect("activate", cb)
+                action.set_accel_group(accelgroup)
+                action.connect_accelerator()
+                toolitem = action.create_tool_item()
+                fake_toolbar.insert(toolitem, -1)
+            fake_toolbar.set_visible(False)
 
     def refresh_sensitivity(self):
         logger.debug('you should implement this in your subclass')
