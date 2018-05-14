150dc82c22b6699f1378a20cd946c2ac079c83e6
mfrasca <mario@anche.no>
2018-05-13 22:15:50 -0500

    trying to produce debian files in a controlled way

---
+++ scripts/makedeb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# make sure we are in the project root dir
+cd $(dirname $0)/..
+
+# grab version from desktop file
+VERSION=$(sed -ne 's/Version=[^0-9]*\([0-9\.]*\).*/\1/p' data/ghini.desktop)
+DIRNAME=bauble-$VERSION
+
+# make sure target dir is clean
+rm -fr $DIRNAME 2>/dev/null
+
+# copy template to target dir
+cp -a package-template $DIRNAME
+# remove placeholders, needed for storing directory structure in git, 
+find $DIRNAME -name ".placeholder" -exec rm {} \;
+# set version in control file
+sed -i 's/VERSION/'$VERSION'/g' $DIRNAME/DEBIAN/control
+
+# copy current files to target dir
+cp -a src/* $DIRNAME/
+cp -a build/lib*/bauble $DIRNAME/usr/share/
+cp -a build/share/* $DIRNAME/usr/share/
+cp -a ./scripts/ghini $DIRNAME/usr/bin/
+
+# don't distribute emacs backup files
+find $DIRNAME -name "*~" -exec rm {} \;
+
+# HERE we make the deb file - files inside deb must be owned by root
+fakeroot bash -c 'chown -R root.root bauble-'$VERSION'; dpkg -b bauble-'$VERSION
+
+# target dir is really a temporary dir, so remove it now
+rm -fr $DIRNAME 2>/dev/null
