41c04059d7d2e651afda4e161bbd4dce85b76772
mfrasca <mario@anche.no>
2018-05-28 11:35:20 -0500

    initially migrating data with bash

---
+++ scripts/bauble-upgrade-1.0-to-3.1.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# this is a simple text conversion
+#
+
+rm -fr 3.1 2>/dev/null
+mkdir 3.1
+cp *.txt 3.1
+cd 3.1
+mv geography.txt geographic_area.txt
+sed -i '1 s/geography/geographic_area/g' *.txt
+sed -i 's/,DATETIME/,'$(date --utc --iso=minutes)'/g' *.txt
+sed -i 's/^version,1\.0\.[0-9]*,/version,3.1.0,/' bauble.txt
+
