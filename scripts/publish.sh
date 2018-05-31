31e1f26634977fb55c3e458a6cf39c538351ea67
mfrasca <mario@anche.no>
2018-05-31 11:37:58 -0500

    publish from active line

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -3,31 +3,30 @@
 # make sure we are in the project root dir
 cd $(dirname $0)/..
 
+# LINE is hard-coded and committed
+# PUBLISHING is in the form 1.0.x
+#
+LINE=ghini-1.0
+PUBLISHING=$(grep :bump bauble/version.py | grep -o '[1-9]\.[0-9]\.[0-9]*')
+
 # let's check what Debian says first
 python setup.py sdist | awk 'BEGIN{count=0}/^.*$/{count++; printf("running setup sdist: %d\r", count)}END{printf("\r\n")}'
-VERSION=$(ls dist/*.tar.gz | tail -n 1 | sed -ne 's/[^-]*-\(.*\).tar.gz/\1/p')
-cp dist/ghini.desktop-${VERSION}.tar.gz /tmp/ghini.desktop-${VERSION}.orig.tar.gz
+cp dist/ghini.desktop-${PUBLISHING}.tar.gz /tmp/ghini.desktop-${PUBLISHING}.orig.tar.gz
 ( cd /tmp
-  rm -fr ghini.desktop-${VERSION}/ 2>/dev/null
-  tar zxf ghini.desktop-${VERSION}.orig.tar.gz 
-  cd ghini.desktop-${VERSION}/
-  dh_make --yes --indep --file ../ghini.desktop-${VERSION}.orig.tar.gz )
-cp debian/* /tmp/ghini.desktop-${VERSION}/debian
-( cd /tmp/ghini.desktop-${VERSION}/
+  rm -fr ghini.desktop-${PUBLISHING}/ 2>/dev/null
+  tar zxf ghini.desktop-${PUBLISHING}.orig.tar.gz 
+  cd ghini.desktop-${PUBLISHING}/
+  dh_make --yes --indep --file ../ghini.desktop-${PUBLISHING}.orig.tar.gz )
+cp debian/* /tmp/ghini.desktop-${PUBLISHING}/debian
+( cd /tmp/ghini.desktop-${PUBLISHING}/
   find debian -iname "*.ex" -execdir rm {} \; -or -name "*.source" -execdir rm {} \; -or -name "*~" -execdir rm {} \;
   debuild )
 
 # decide whether we continue
 # in case, we should really dput the following to mentors.debian.org
 echo
-echo dput mentors $(ls /tmp/ghini.desktop_${VERSION}-*_*.changes | tail -n 1)
+echo dput mentors $(ls /tmp/ghini.desktop_${PUBLISHING}-*_*.changes | tail -n 1)
 echo
-
-# LINE is hard-coded and committed
-# PUBLISHING is in the form 1.0.x
-#
-LINE=ghini-1.0
-PUBLISHING=$(grep :bump bauble/version.py | grep -o '[1-9]\.[0-9]\.[0-9]*')
 
 # make sure you have locally all remote branches
 #
