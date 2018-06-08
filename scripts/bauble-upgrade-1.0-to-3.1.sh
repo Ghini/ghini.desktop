fb5db14cfad159092a508e44a6d0291c473babb2
mfrasca <mario@anche.no>
2018-06-08 14:43:34 -0500

    complete #380: also correct the export.

--- scripts/bauble-upgrade-1.0-to-3.1.sh
+++ scripts/bauble-upgrade-1.0-to-3.1.sh
@@ -14,6 +14,11 @@ mv source_detail.txt contact.txt
 sed -i '1 s/source_detail/contact/g' *.txt
 sed -i '1 s/geography/geographic_area/g' *.txt
 
+sed -i '1 s/family,/epithet,/' family.txt
+sed -i '1 s/genus,/epithet,/' genus.txt
+sed -i '1 s/sp,/epithet,/' species.txt
+sed -i '1 s/sp_author,/author,/' species.txt
+
 sed -i 's/,DATETIME/,'$(date --utc --iso=minutes)'/g' *.txt
 sed -i 's/^version,1\.0\.[0-9]*,/version,3.1.0,/' bauble.txt
 
