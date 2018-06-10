fbe79781f58a1d25cb59dbbb8e2edf00e220066d
mfrasca <mario@anche.no>
2018-06-10 14:42:21 -0500

    remove my messy debian interpretation.
    I should review the comments I received from Mattia Rizzolo.

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -5,21 +5,8 @@ cd $(dirname $0)/..
 
 # let's check what Debian says first
 python setup.py sdist | awk 'BEGIN{count=0}/^.*$/{count++; printf("running setup sdist: %d\r", count)}END{printf("\r\n")}'
-VERSION=$(ls dist/*.tar.gz | tail -n 1 | sed -ne 's/[^-]*-\(.*\).tar.gz/\1/p')
-cp dist/ghini.desktop-${VERSION}.tar.gz /tmp/ghini.desktop-${VERSION}.orig.tar.gz
-( cd /tmp
-  rm -fr ghini.desktop-${VERSION}/ 2>/dev/null
-  tar zxf ghini.desktop-${VERSION}.orig.tar.gz 
-  cd ghini.desktop-${VERSION}/
-  dh_make --yes --indep --file ../ghini.desktop-${VERSION}.orig.tar.gz )
-cp debian/* /tmp/ghini.desktop-${VERSION}/debian
-( cd /tmp/ghini.desktop-${VERSION}/
-  find debian -iname "*.ex" -execdir rm {} \; -or -name "*.source" -execdir rm {} \; -or -name "*~" -execdir rm {} \;
-  debuild )
-
-# decide whether we continue
-# in case, we should really dput the following to mentors.debian.org
-echo dput mentors $(ls /tmp/ghini.desktop_${VERSION}-*_*.changes | tail -n 1)
+# debian needs a frozen upstream, on which to base its packaging.  a good
+# way to freeze might mean starting a branch at the merge point.
 
 # LINE is hard-coded and committed
 # PUBLISHING is in the form 3.1.x
@@ -39,9 +26,7 @@ git push
 
 # publish on pypi
 #
-echo python setup.py sdist --formats zip upload -r pypi
-
-# some day also produce a debian package
+echo git checkout ghini-3.1 && python setup.py sdist --formats zip upload -r pypi
 
 # some day also produce a windows installable
 
