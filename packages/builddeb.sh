b2cdd68719d95e7d74d95a1b0bd8f4832d7095ab
mfrasca <mario@anche.no>
2018-09-15 06:32:11 -0500

    "bumping_to_3.1.7"

--- packages/builddeb.sh
+++ packages/builddeb.sh
@@ -3,7 +3,7 @@
 # Requires bzr, devscripts, debhelper packages
 
 TOPLEVEL=`pwd`
-VERSION="3.1.6" # :bump
+VERSION="3.1.7" # :bump
 TARBALL="bauble-$VERSION.tar.gz"
 ORIG_TARBALL="bauble_$VERSION.orig.tar.gz"
 
