6d16f657e471cdf5243714c1880b296b9b725ffe
mfrasca <mario@anche.no>
2018-09-28 14:41:06 -0500

    see #419

--- scripts/devinstall.sh
+++ scripts/devinstall.sh
@@ -5,67 +5,66 @@
 
 while true
 do
-
-    PROBLEMS=''
+    MISSING=''
     if ! msgfmt --version >/dev/null 2>&1; then
-        PROBLEMS="$PROBLEMS gettext"
+        MISSING="$MISSING gettext"
     fi
     if ! python3 --version >/dev/null 2>&1; then
-        PROBLEMS="$PROBLEMS python3-minimal"
+        MISSING="$MISSING python3-minimal"
     fi
     if ! python3 -c 'import gi' >/dev/null 2>&1; then
-        PROBLEMS="$PROBLEMS python3-gi"
+        MISSING="$MISSING python3-gi"
     fi
     if ! python3 -c 'import gi; gi.require_version("Clutter", "1.0"); gi.require_version("GtkClutter", "1.0"); from gi.repository import Clutter, GtkClutter; ' >/dev/null 2>&1; then
-        PROBLEMS="$PROBLEMS gir1.2-gtkclutter "
+        MISSING="$MISSING gir1.2-gtkclutter "
     fi
     if ! python3 -c 'import gi; gi.require_version("Clutter", "1.0"); gi.require_version("GtkClutter", "1.0"); from gi.repository import Clutter, GtkClutter; gi.require_version("Champlain", "0.12"); from gi.repository import GtkChamplain; GtkClutter.init([]); from gi.repository import Champlain' >/dev/null 2>&1; then
-        PROBLEMS="$PROBLEMS gir1.2-gtkchamplain-0.12 "
+        MISSING="$MISSING gir1.2-gtkchamplain-0.12 "
     fi
     if ! python3 -c 'import lxml' >/dev/null 2>&1; then
-        PROBLEMS="$PROBLEMS python3-lxml"
+        MISSING="$MISSING python3-lxml"
     fi
     if ! git help >/dev/null 2>&1; then
-        PROBLEMS="$PROBLEMS git"
+        MISSING="$MISSING git"
     fi
     if ! virtualenv --help >/dev/null 2>&1; then
-        PROBLEMS="$PROBLEMS virtualenv"
+        MISSING="$MISSING virtualenv"
     fi
     if ! xslt-config --help >/dev/null 2>&1; then
-        PROBLEMS="$PROBLEMS libxslt1-dev"
+        MISSING="$MISSING libxslt1-dev"
     fi
     if ! pkg-config --help >/dev/null 2>&1; then
-        PROBLEMS="$PROBLEMS pkg-config"
+        MISSING="$MISSING pkg-config"
     fi
     if ! pkg-config --cflags jpeg --help >/dev/null 2>&1; then
-        PROBLEMS="$PROBLEMS libjpeg-dev"
+        MISSING="$MISSING libjpeg-dev"
     fi
     if ! gcc --version >/dev/null 2>&1; then
-        PROBLEMS="$PROBLEMS build-essential"
+        MISSING="$MISSING build-essential"
     fi
     PYTHONHCOUNT=$(find /usr/include/python3* /usr/local/include/python3* -name Python.h 2>/dev/null | wc -l)
     if [ "$PYTHONHCOUNT" = "0" ]; then
-        PROBLEMS="$PROBLEMS libpython3-all-dev"
+        MISSING="$MISSING libpython3-all-dev"
     fi
 
     # forget password, please.
     sudo -k
 
-    if [ "$PROBLEMS" == "" ]
+    if [ "$MISSING" == "" ]
     then
         break;
     else
         echo 'Guessing package names, if you get in a loop, please double check.'
         echo 'You need to solve the following dependencies:'
         echo '------------------------------------------------------------------'
-        echo $PROBLEMS
+        echo $MISSING
         echo '------------------------------------------------------------------'
         echo 'Then restart the devinstall.sh script'
         if [ -x /usr/bin/apt-get ]; then
             echo
             echo 'you are on a debian-like system, I should know how to install'
-            echo $PROBLEMS
-            sudo apt-get -y install $PROBLEMS
+            echo $MISSING
+            sudo apt-get -y install $MISSING
             echo -n 'press <ENTER> to re-run devinstall.sh, or Ctrl-C to stop'
             read
         fi
@@ -75,27 +74,31 @@ done
 if [ -d $HOME/Local/github/Ghini/ghini.desktop ]
 then
     echo "ghini checkout already in place"
-    cd $HOME/Local/github/Ghini/ghini.desktop
+    cd $HOME/Local/github/Ghini
 else
     mkdir -p $HOME/Local/github/Ghini >/dev/null 2>&1
     cd $HOME/Local/github/Ghini
     git clone https://github.com/Ghini/ghini.desktop
-    cd ghini.desktop
 fi
+cd ghini.desktop
 
 if [ $# -ne 0 ]
 then
-    git checkout ghini-$1
+    VERSION=$1
+    LINE=ghini-$1
 else
-    git checkout ghini-3.1
+    VERSION=3.1
+    LINE=ghini-3.1
 fi
 
+git checkout $LINE
+
 mkdir -p $HOME/.virtualenvs
-virtualenv --python python3 $HOME/.virtualenvs/ghide --system-site-packages
-find $HOME/.virtualenvs/ghide -name "*.pyc" -or -name "*.pth" -execdir rm {} \;
-mkdir -p $HOME/.virtualenvs/ghide/share
+virtualenv --python python3 $HOME/.virtualenvs/$LINE --system-site-packages
+find $HOME/.virtualenvs/$LINE -name "*.pyc" -or -name "*.pth" -execdir rm {} \;
+mkdir -p $HOME/.virtualenvs/$LINE/share
 mkdir -p $HOME/.ghini
-. $HOME/.virtualenvs/ghide/bin/activate
+. $HOME/.virtualenvs/$LINE/bin/activate
 
 if [ ! -z $PG ]
 then
@@ -116,7 +119,7 @@ cat <<EOF > $HOME/bin/ghini
 #!/bin/bash
 
 GITHOME=$HOME/Local/github/Ghini/ghini.desktop/
-. \$HOME/.virtualenvs/ghide/bin/activate
+. \$HOME/.virtualenvs/$LINE/bin/activate
 
 while getopts us:mp f
 do
@@ -160,12 +163,12 @@ echo enter your password to make Ghini available to other users.
 
 sudo groupadd ghini 2>/dev/null 
 sudo usermod -a -G ghini $(whoami)
-chmod -R g-w+rX,o-rwx $HOME/.virtualenvs/ghide
-sudo chgrp -R ghini $HOME/.virtualenvs/ghide
+chmod -R g-w+rX,o-rwx $HOME/.virtualenvs/$LINE
+sudo chgrp -R ghini $HOME/.virtualenvs/$LINE
 cat <<EOF | sudo tee /usr/local/bin/ghini > /dev/null
 #!/bin/bash
-. $HOME/.virtualenvs/ghide/bin/activate
-$HOME/.virtualenvs/ghide/bin/ghini
+. $HOME/.virtualenvs/$LINE/bin/activate
+$HOME/.virtualenvs/$LINE/bin/ghini
 EOF
 sudo chmod +x /usr/local/bin/ghini
 
@@ -175,9 +178,9 @@ cat <<EOF | sudo tee /usr/local/share/applications/ghini.desktop > /dev/null
 [Desktop Entry]
 Type=Application
 Name=Ghini Desktop
-Version=3.1
+Version=$VERSION
 GenericName=Biodiversity Manager
-Icon=$HOME/.virtualenvs/ghide/share/icons/hicolor/scalable/apps/ghini.svg
+Icon=$HOME/.virtualenvs/$LINE/share/icons/hicolor/scalable/apps/ghini.svg
 TryExec=/usr/local/bin/ghini
 Exec=/usr/local/bin/ghini
 Terminal=false
