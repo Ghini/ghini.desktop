cdf837df8c0e626f3bf8fabb9c05ddc901d02a0c
mfrasca <mario@anche.no>
2018-10-10 17:16:06 -0500

    removing repeated dot

--- bauble/plugins/report/utils.py
+++ bauble/plugins/report/utils.py
@@ -464,7 +464,6 @@ class Code39:
            '4': 'b b   bbb b bbb',
            'X': 'b   b bbb b bbb',
            '0': 'b b   bbb bbb b',
-           '.': 'bbb   b b bbb b',
            '1': 'bbb b   b b bbb',
            '3': 'bbb bbb   b b b',
            '2': 'b bbb   b b bbb',
