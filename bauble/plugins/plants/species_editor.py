c36218351cadb6563d3dcdffb0b1a9552d77644d
Mario Frasca <mario.frasca@comune.trapani.it>
2026-06-15 15:33:53 +0200

    chore: just more uniform and compact alignments

--- bauble/plugins/plants/species_editor.py
+++ bauble/plugins/plants/species_editor.py
@@ -1488,15 +1488,13 @@ class SpeciesEditorView(editor.GenericEditorView):
     def connect_actions(self):
         dialog = self.get_window()
         self.widgets.sp_cancel_button.connect(
-            "clicked", lambda b: dialog.response(Gtk.ResponseType.CANCEL)
-        )
+            "clicked", lambda b: dialog.response(Gtk.ResponseType.CANCEL))
         self.widgets.sp_ok_button.connect(
-            "clicked", lambda b: dialog.response(Gtk.ResponseType.OK)
-        )
+            "clicked", lambda b: dialog.response(Gtk.ResponseType.OK))
         self.widgets.sp_ok_and_add_button.connect(
-            "clicked", lambda b: dialog.response(11)
-        )
-        self.widgets.sp_next_button.connect("clicked", lambda b: dialog.response(22))
+            "clicked", lambda b: dialog.response(11))
+        self.widgets.sp_next_button.connect(
+            "clicked", lambda b: dialog.response(22))
 
     @staticmethod
     def genus_completion_cell_data_func(
