6d5da068fefbf54da28057a2aff7e0145934be86
Mario Frasca <mario.frasca@comune.trapani.it>
2026-06-15 16:13:05 +0200

    chore(debug): one event => one warning.
    do not produce two distinct warning for the same thing.
    consider producing one warning plus extra information at lower level.

--- bauble/prefs.py
+++ bauble/prefs.py
@@ -217,10 +217,8 @@ class _prefs(dict):
             self.config.read(self._filename)
         version = self[config_version_pref]
         if version is None:
-            logger.warning(f"{self._filename} has no config version pref")
-            logger.warning(
-                "setting the config version to {}.{}".format(*config_version)
-            )
+            logger.warning("%s has no config version pref; setting the config version to %s.%s",
+                           self._filename, *config_version)
             self[config_version_pref] = config_version
 
         # set some defaults if they don't exist
